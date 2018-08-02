f25f50350600c581d3bbc19819f666df23b605b7
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2018-08-02 22:18:16 +0900

    fix .github_release.sh

--- .github_release.sh
+++ .github_release.sh
@@ -3,12 +3,15 @@
 set -e
 #set -x
 
-git clone -b v2.2.2 https://github.com/github/hub.git;
-(cd hub; ./script/build; sudo cp hub /usr/local/bin/)
+# install latest hub 2017.08.04
+git clone -b v2.3.0-pre10 https://github.com/github/hub.git
+(cd hub; ./script/build; sudo cp bin/hub /usr/local/bin/)
 
 mkdir -p ~/.config/
 echo "github.com:" > ~/.config/hub
 echo "- user: k-okada" >> ~/.config/hub
 echo "  oauth_token: $GITHUB_ACCESS_TOKEN" >> ~/.config/hub
+
+hub release; sleep 5
 hub release create -p -a ROScheatsheet_catkin.pdf -m "$CIRCLE_TAG"$'\n'"Released on `date '+%Y/%m/%d %H:%M:%S'`" $CIRCLE_TAG;
 
